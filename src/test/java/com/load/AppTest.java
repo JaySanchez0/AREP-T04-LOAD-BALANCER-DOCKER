0cd8267f1aab1c6efb953c1168e11bec76dc49dc
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-09-18 18:24:48 -0500

    circle ci

--- src/test/java/com/load/AppTest.java
+++ src/test/java/com/load/AppTest.java
@@ -1,16 +1,15 @@
 package com.load;
 
 import com.load.request.HTTPPetition;
-import com.load.response.HttpResponse;
+import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import spark.Spark;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -25,27 +24,63 @@ public class AppTest {
         app.operations();
     }
 
+    /**
+     * Valida que devuelva un html
+     * @throws Exception
+     */
     @Test
-    public void shouldBeRecibeHtml() throws UnirestException {
-        System.out.println(Unirest.get("http://localhost:3032").asString().getBody().toString());
-        HttpResponse resp = new HTTPPetition().get("http://127.0.0.1:3032");
-        assertEquals(resp.getContentType(),"text/html");
+    public void shouldBeRecibeHtml() throws Exception {
+        HttpResponse<String> res = Unirest.get("http://localhost:3032").asString();
+        assertEquals(res.getHeaders().getFirst("Content-Type"),"text/html");
     }
 
+    /**
+     * Valida que devuelva un objeto json
+     * @throws Exception
+     */
     @Test
-    public void shouldBeRecibeJsonObject(){
-        HttpResponse resp = new HTTPPetition().get("http://127.0.0.1:3032/note");
-        assertEquals(resp.getContentType(),"application/json");
+    public void shouldBeRecibeJsonObject() throws Exception{
+        HttpResponse<String> res = Unirest.get("http://localhost:3032/note").asString();
+        assertEquals(res.getHeaders().getFirst("Content-Type"),"application/json");
     }
 
+    /**
+     * Valida que reciba un archivo javascript
+     * @throws Exception
+     */
     @Test
-    public void shouldBeRecibeAJavascriptFile(){
-        HttpResponse resp = new HTTPPetition().get("http://127.0.0.1:3032/js/app.js");
-        System.out.println(resp.getBody());
-        assertEquals(resp.getContentType(),"application/javascript");
+    public void shouldBeRecibeAJavascriptFile() throws Exception{
+        HttpResponse<String> res = Unirest.get("http://localhost:3032/js/app.js").asString();
+        assertEquals(res.getHeaders().getFirst("Content-Type"),"application/javascript");
     }
 
+    private int getNoteOcuurences() throws Exception{
+        String res = Unirest.get("http://localhost:3032/note").asString().getBody().toString();
+        System.out.println(res);
+        JSONArray array = new JSONArray(res);
+        int cont = 0;
+        for(int i=0;i<array.length();i++){
+            JSONObject ob = array.getJSONObject(i);
+            if(ob.getString("work").equals("testw") && ob.getString("description").equals("test")) cont+=1;
+        }
+        return cont;
+    }
 
+    /**
+     * Valida que agregue satisfactoriamente una nueva nota
+     * @throws Exception
+     */
+    @Test
+    public void shouldBeAddAnNewNote() throws Exception{
+        int c1 = getNoteOcuurences();
+        Unirest.post("http://localhost:3032/note").header("Content-Type","application/json").body("{\"work\":\"testw\",\"description\":\"test\"}").asString().getBody();
+        int c2 = getNoteOcuurences();
+        assertTrue(c2>c1);
+    }
+
+    /**
+     * Detiene el balanceador de carga
+     */
     @AfterClass
     public static void stop(){
         Spark.stop();
